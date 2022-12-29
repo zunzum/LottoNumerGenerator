87cfae17daa32153e07dc5af264c5002ba8584fd
zunzum <zunzum@naver.com>
2022-12-29 21:20:57 +0900

    변수 실습

---
+++ app/src/main/java/com/zunzum/lottonumergenerator/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.zunzum.lottonumergenerator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zunzum.lottonumergenerator.ui.theme.LottoNumerGeneratorTheme
+
+fun main() {
+
+    var name = "동준이"
+    name = "히히"
+
+    println(name)
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LottoNumerGeneratorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("동준이")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LottoNumerGeneratorTheme {
+        Greeting("Android")
+    }
+}
